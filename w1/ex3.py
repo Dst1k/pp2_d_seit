f8b0b7791cec8e6bbf8f55f62d4bd45be14719ef
Dst1k <dastanseit01@gmail.com>
2026-01-20 12:16:56 +0500

    example three

---
+++ w1/ex3.py
@@ -0,0 +1,8 @@
+#python casting
+x = int(1)#output 1
+y = int(2.8)#output 2
+z = int("3")#output 3
+a = float(3)#output 3.0
+b = float("4")#output 4.0
+c = str("2s") #output 2s
+d = str(4) #output 4
