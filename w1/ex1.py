8ad2cc68a6a689d2198c11b39dfe19cebe2bcd07
Dst1k <dastanseit01@gmail.com>
2026-01-20 12:02:19 +0500

    example one

---
+++ w1/ex1.py
@@ -0,0 +1,8 @@
+#this is a comment, further i will use it to show the "title" and the topic of an example
+#python output, printing texts and numbers
+print("hello world!")
+print("programming is great")
+print("watch breaking bad", end=" ")
+print("it's peak")
+print("6" + "7")
+print(6+7)
