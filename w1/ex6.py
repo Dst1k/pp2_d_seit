7085652d057260a8a473a63191549c661cdd9ae3
Dst1k <dastanseit01@gmail.com>
2026-01-20 16:41:13 +0500

    example 6

---
+++ w1/ex6.py
@@ -0,0 +1,8 @@
+#python is used for:
+#web development
+#software development
+#maths
+#system scripting
+#it can:
+#create applications, games, connect databases, modify files,etc
+#it is good beacause it works on different platforms and its relatively simple
