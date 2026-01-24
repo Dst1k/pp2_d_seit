53106849780c61db1f0dcebe10b1040f44b12a71
Dst1k <dastanseit01@gmail.com>
2026-01-24 15:09:26 +0500

    example seven

---
+++ w1/ex7.py
@@ -0,0 +1,10 @@
+#to check python version you must summon the command line and input
+#"py --version"
+#it also shows whether the python is installed or not
+#to run the code you simply have to type
+#"py nameofcode.py"
+import sys
+print(sys.version)
+#if you write
+#"py" or "python" on your command line you can write code inside the command line
+#then you can leave by typing "exit()"
