8b5173fad44f0ec66fa266d0a50b67f66a1af1bd
Dst1k <dastanseit01@gmail.com>
2026-01-24 15:21:17 +0500

    last example

---
+++ w1/ex9.py
@@ -0,0 +1,12 @@
+import random
+x = 1
+y = 2.8
+z = 1j
+print(type(x),type(y),type(z))
+#convertion
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a,b,c)
+print(type(a),type(b),type(c))
+print(random.randrange(1,10))
