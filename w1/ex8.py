eb108c43389d8efb632f7d02c9a02927f0e7400e
Dst1k <dastanseit01@gmail.com>
2026-01-24 15:16:33 +0500

    example eight

---
+++ w1/ex8.py
@@ -0,0 +1,10 @@
+if 5 > 2:
+print("5 is greater than 2") #syntax error
+if 5 > 2:
+    print("5 is greater than 2") #as many spaces as you want
+#however not like this
+if 5 > 2:
+    print("whatever")
+      print("whatever") #error
+#also you cant do this
+print("hello") print("world")
