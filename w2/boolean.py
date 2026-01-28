ed411e05886dd38dea7a333d4391f93c903627f6
Dst1k <dastanseit01@gmail.com>
2026-01-28 15:53:27 +0500

    boolean

---
+++ w2/boolean.py
@@ -0,0 +1,9 @@
+#booleans are true or false values:
+print(10 > 9, 10 < 9)
+#they are often a result of comparisons and logical operators:
+if 10 > 9:
+    print("true")
+else:
+    print("false")
+bool() # returns true most of the times,except if the value is empty or 0
+isinstance(x,y) #x - a value, y - type of a value. Returns true if type of x matches y
