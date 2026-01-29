fea67a362d00555ad1a27b34c73add1b85ef6913
Dst1k <dastanseit01@gmail.com>
2026-01-29 19:44:23 +0500

    bool operators and shorthand if

---
+++ w2/shorthandif.py
@@ -0,0 +1,46 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+#logical operators combined with if
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+age = 25
+is_student = False
+has_discount_code = True
+
+if (age < 18 or age > 65) and not is_student or has_discount_code:
+  print("Discount applies!")
+
+#Nested if
+x = 41
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
