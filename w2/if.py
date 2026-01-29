989c3bd901f48ec73a163035c30968fee16b7a55
Dst1k <dastanseit01@gmail.com>
2026-01-29 19:29:03 +0500

    if

---
+++ w2/if.py
@@ -0,0 +1,20 @@
+#python conditions and if statements
+a = 1
+b = 2
+a == b #a equals b
+a != b #a doesnt equal b
+a < b #a is less than b
+a > b #a is greater than b
+a <= b # less than or equal to
+a >= b # more than or equal to
+x = 33
+y = 200
+if x < y:
+    print("x is less than y")
+v = 9
+if v%3 == 0:
+    print("Yes")
+    print("Its true") #multiple statements
+c = True
+if c:
+    print("yeah true bradar") #boolean as a variable, everything besides 0,"" and None returns true
